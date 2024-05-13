222ef43e8c2266f4826f7113711b1f26435051e4
KilianAlves <alveskilian@gmail.com>
2024-05-13 14:06:16 +0000

    test: add each function to test multiple value for the square route

--- tests/hello/hello.test.ts
+++ tests/hello/hello.test.ts
@@ -1,5 +1,6 @@
 import supertest from 'supertest';
 import { app } from '../../src/app';
+import { test } from '@jest/globals';
         
 describe('Test /api/hello', () => {
     test('GET /api/hello/world', async () => {
@@ -7,9 +8,15 @@ describe('Test /api/hello', () => {
         expect(response.statusCode).toBe(200);
         expect(response.body.message).toBe('hello');
     });
-    test('GET /api/hello/square/5', async () => {
-        const response = await supertest(app).get('/api/hello/square/5');
+    test.each([
+        {num: 1, result: 1},
+        {num: 4, result: 16},
+        {num: 5, result: 25},
+        {num: 10, result: 100},
+        {num: 12, result: 144}
+    ])('GET /api/hello/square/5', async ({num, result}) => {
+        const response = await supertest(app).get('/api/hello/square/' + num);
         expect(response.statusCode).toBe(200);
-        expect(response.body).toEqual({ result: 25 });
+        expect(response.body).toEqual({ result: result });
     });
 });
