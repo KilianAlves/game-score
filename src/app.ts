5127e6105eea9cce0f844f41f5a41051b6de744d
KilianAlves <alveskilian@gmail.com>
2024-05-13 12:41:24 +0000

    code: add code fot the test hello

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,7 @@
 import express, { Request, Response, NextFunction } from 'express';
         
 export const app = express();
+
+app.get('/api/hello/world', (req: Request, res: Response, next: NextFunction) => {
+    res.status(200).json({ message: 'Hello, world!' });
+});
