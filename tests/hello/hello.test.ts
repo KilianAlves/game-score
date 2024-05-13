54809f65066d4a372a31249e3a9133223a83c280
KilianAlves <alveskilian@gmail.com>
2024-05-13 13:43:47 +0000

    test: add test to generate the square of a num

--- tests/hello/hello.test.ts
+++ tests/hello/hello.test.ts
@@ -7,4 +7,9 @@ describe('Test /api/hello', () => {
         expect(response.statusCode).toBe(200);
         expect(response.body.message).toBe('hello');
     });
+    test('GET /api/hello/square/5', async () => {
+        const response = await supertest(app).get('/api/hello/square/5');
+        expect(response.statusCode).toBe(200);
+        expect(response.body).toEqual({ result: 25 });
+    });
 });
