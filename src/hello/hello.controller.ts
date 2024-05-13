3d46de9ca1f4ef8bfde40c6ab4cc367557832202
KilianAlves <alveskilian@gmail.com>
2024-05-13 14:13:18 +0000

    code: add verification on square route to avoid no number attribute

--- src/hello/hello.controller.ts
+++ src/hello/hello.controller.ts
@@ -6,6 +6,10 @@ export class HelloController {
     }
 
     public getSquare(req: Request, res: Response): void {
+        if (isNaN(parseInt(req.params.num))) {
+            res.status(404).json({ message: 'Invalid input, number required' });
+            return;
+        }
         const num: number = parseInt(req.params.num);
         res.status(200).json({ result: num * num });
     }
