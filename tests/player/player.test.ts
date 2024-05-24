a29562c8ed8f5dc718d610e9c0d40c2b684b0369
KilianAlves <alveskilian@gmail.com>
2024-05-24 11:54:52 +0000

    test: add test for api/player/ get

--- tests/player/player.test.ts
+++ tests/player/player.test.ts
@@ -6,17 +6,17 @@ import { playerRepository } from "../../src/player/player.repository"
 
 describe('Test /api/player', () => {
     test('GET /api/player/', async () => {
-        // await playerRepository.clear();
-        // await playerRepository.insert(
-        //     { message: "hello" },
-        //     { message: "world" },
-        // ); 
+        await playerRepository.clear();
+        await playerRepository.insert(
+            { firstName: "Barry", lastName: "Allen", tour: "ATP", country:"USA" },
+            { firstName: "Eobard", lastName: "Thawn", tour: "ATP", country:"USA" },
+        ); 
         
         const response = await supertest(app).get("/api/player");
         
         expect(response.statusCode).toBe(200); 
         expect(response.body.length).toEqual(2);
-        // expect(response.body[0].message).toEqual("hello");
-        // expect(response.body[1].message).toEqual("world"); 
+        expect(response.body[0].firstName).toEqual("Barry");
+        expect(response.body[1].firstName).toEqual("Eobard"); 
     });
 })
